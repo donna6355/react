a04cbb0ca18ac5e647d3d5956a200b85620c3125
Donna:) <71857113+donna6355@users.noreply.github.com>
2020-10-08 20:43:31 +0900

    Add files via upload

---
+++ precache-manifest.3690ba1ecc53d11bc8cb8723df6a4216.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f4ac34f24b58072c6a81c7ae7a3229",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3389eb0ec529da3c9b5",
+    "url": "/static/js/2.05fb6b8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05fb6b8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b35447285ff53309ebc4",
+    "url": "/static/js/main.561a4c25.chunk.js"
+  },
+  {
+    "revision": "df40ba753f8222cfb8ed",
+    "url": "/static/js/runtime-main.0cd3ba68.js"
+  }
+]);
