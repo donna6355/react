264334e9f7c08ca9337de70cb8c02313a3dcabb9
Donna:) <71857113+donna6355@users.noreply.github.com>
2020-10-16 11:04:22 +0900

    Add files via upload

---
+++ shoppinglist build/precache-manifest.90050120b4ca54c74999d33d5488b928.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3a86b2ad4cf05e9310bb541c7025f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "104797d2fc171da7a921",
+    "url": "/static/css/main.51d2b3ad.chunk.css"
+  },
+  {
+    "revision": "3012e62448aaf9132374",
+    "url": "/static/js/2.fa45f861.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.fa45f861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "104797d2fc171da7a921",
+    "url": "/static/js/main.249b6c16.chunk.js"
+  },
+  {
+    "revision": "7d132928e29fbc91b79f",
+    "url": "/static/js/runtime-main.8335ea04.js"
+  },
+  {
+    "revision": "7d6ed49107de3aead217cff0d9029603",
+    "url": "/static/media/react_kermit.7d6ed491.png"
+  }
+]);
